f3ff53c9d493b97422aac03bd903e81b9bbae394
jindalsh <jindalsh@deakin.edu.au>
2019-03-29 14:57:25 +1100

    Initialised the XCode Project

--- changelog.md
+++ changelog.md
@@ -1,4 +1,4 @@
 
 # Friday, Week 4, 29/March
-- Initialise the app. [who??]
+- Initialise the app. [Shubham Jindal, jindalsh, 218053165]
 
---
+++ code/ImageRecognitionApp/ImageRecognitionApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageRecognitionApp
+//
+//  Created by Shubham Jindal on 29/03/19.
+//  Copyright © 2019 sjc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
