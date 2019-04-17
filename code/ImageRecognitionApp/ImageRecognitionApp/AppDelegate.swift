5bd9b9934c43bd035ec44858507cce9760164b6f
Sohil Kaushal <kaushals@deakin.edu.au>
2019-04-17 21:40:28 +1000

    Started Core data and fixed Splash screen

--- code/ImageRecognitionApp/ImageRecognitionApp/AppDelegate.swift
+++ code/ImageRecognitionApp/ImageRecognitionApp/AppDelegate.swift
@@ -18,7 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var savedObjects = [String]()
     
     var historyObjects: [NSManagedObject] = []
-
+    var listObjects: [NSManagedObject] = []
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
